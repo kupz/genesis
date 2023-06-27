9295d54a7b55975ed2a72ac33ed3577b0786a822
Kupz <benjohnvillabesa@gmail.com>
2023-06-27 15:53:46 +0800

    added countdown

---
+++ src/components/CountdownTimer.js
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from "react";
+
+const CountdownTimer = () => {
+  const [countdown, setCountdown] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  });
+
+  useEffect(() => {
+    const countdownDate = new Date("July 8, 2023 09:00:00").getTime();
+
+    const interval = setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countdownDate - now;
+
+      const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+      const hours = Math.floor(
+        (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+      setCountdown({ days, hours, minutes, seconds });
+
+      if (distance < 0) {
+        clearInterval(interval);
+        setCountdown({ days: 0, hours: 0, minutes: 0, seconds: 0 });
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div>
+      <div>{countdown.days} days</div>
+      <div>{countdown.hours} hours</div>
+      <div>{countdown.minutes} minutes</div>
+      <div>{countdown.seconds} seconds</div>
+    </div>
+  );
+};
+
+export default CountdownTimer;
---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import "./navbar.scss";
+export default function Navbar() {
+  return (
+    <div className="nav-container">
+      <div>
+        <span>Birthday</span>
+      </div>
+      <div>
+        <div>attendees</div>
+        <div>days Left</div>
+        <div></div>
+      </div>
+    </div>
+  );
+}
