323fc1ad14d24e52ee7192b450731a4c683486ee
Kupz <benjohnvillabesa@gmail.com>
2023-06-29 10:56:06 +0800

    almost done

--- src/App.js
+++ src/App.js
@@ -8,22 +8,82 @@ function App() {
     <div className="App">
       <div className="app-countdown">
         <CountdownTimer />
-        <span>before Genesis birthday</span>
+        <div className="nav">
+          <div> Ninong & Ninang</div>
+          <div> Guest List</div>
+        </div>
       </div>
-      <div className="hero1">
-        <div className="left">
-          <p>PLEASE JOIN US</p>
-          <p>AS WE CELEBRATE THE</p>
-          <h2>Christening & Birthday</h2>
-          <p>OF OUR SON</p>
+      <div className="hero">
+        <div className="genesis-container">
+          <img src={genesis} alt="genesis" className="genesis" />
+        </div>
+        <div className="hero-right">
+          <span>PLEASE JOIN US</span>
+          <span>AS WE CELEBRATE</span>
+          <h1>Christening & Birthday</h1>
+          <span>OF OUR SON </span>
           <div>
-            <h1>GENESIS</h1>
-            <h3>07 : 10 - 22</h3>
+            <span>GENESIS</span>
+            <span>7 : 10 - 22</span>
+          </div>
+          <button>Confirm my Attendace</button>
+        </div>
+      </div>
+      {/* When ? */}
+      <div className="when">
+        <div>
+          <div>
+            <h1>When & Where</h1>
+            <span>July 8 , 2023</span>
+
+            <span>420 Burger Ave.</span>
+
+            <address>
+              {
+                " 1358 Claremont St. Brookside Hills , Cainta, Philippines, 1900"
+              }
+            </address>
           </div>
         </div>
-        <div className="genesis-container">
-          <img src={genesis} className="genesis" />
+      </div>
+      {/* DRESSCODE */}
+      <div className="dresscode">
+        <h1>Dress Code</h1>
+        <span>Casual Attire</span>
+        <span>White / Neutral</span>
+        <div className="dressColors">
+          <div className="white"></div>
+          <div className="brown"></div>
+          <div className="others1"></div>
+          <div className="others2"></div>
         </div>
+      </div>
+      {/* gift ideas */}
+      <div className="gift-ideas">
+        <h1>Gift Ideas</h1>
+        <div>
+          <span>
+            YOUR PRAYERS AND PRESENCE ARE ALL THAT WE REQUEST. BUT IF YOU DESIRE
+            TO GIVE NONETHELESS, THESE ARE THINGS THAT WE WOULD REALLY
+            APPRECIATE.
+          </span>
+          <div>
+            <div>Unilab Diaper size : medium</div>
+            <div>Wet wipes & Dry wipes</div>
+            <div>Cleansing Splash & Laundry Powder</div>
+            <div>Educational Books & Learning Toys</div>
+            <div>Monetary gifts for my savings account</div>
+          </div>
+          <span>
+            Just because we gave ideas doesn't mean you are obligated to give.
+            Love, Prayers and Your Presence is the most precious gift you can
+            give.
+          </span>
+        </div>
+      </div>
+      <div className="footer">
+        <h1>Save the date!</h1>
+        <h2>See you on my special Day!</h2>
       </div>
     </div>
   );
